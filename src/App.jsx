db9138bdd2943d4106e4b1c868f6d5599860ee7e
Froggles <elmsterm@gmail.com>
2026-02-11 10:40:56 -0500

    refactor App with routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Register from "./auth/Register";
 import Login from "./auth/Login";
 import ActivitiesPage from "./activities/ActivitiesPage";
 import Error404 from "./Error404.jsx";
+import { Route, Routes } from "react-router";
 
 /**
  * Fitness Trackr is a platform where fitness enthusiasts can share their workouts and
@@ -9,11 +10,12 @@ import Error404 from "./Error404.jsx";
  * account will be able to upload and manage their own activities.
  */
 export default function App() {
-  const page = "";
-
-  if (page === "register") return <Register />;
-  if (page === "login") return <Login />;
-  if (page === "activities") return <ActivitiesPage />;
-
-  return <Error404 />;
+  return (
+    <Routes>
+      <Route path="/" element={<ActivitiesPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+  );
 }
