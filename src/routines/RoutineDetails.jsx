ba077d5e384ab3662a35a3545bf7e4937b062cfa
Froggles <elmsterm@gmail.com>
2026-02-11 13:04:48 -0500

    add SetForm to routine details

--- src/routines/RoutineDetails.jsx
+++ src/routines/RoutineDetails.jsx
@@ -2,20 +2,22 @@ import { Link, useNavigate, useParams } from "react-router";
 import { deleteRoutine, getRoutines } from "../api/routines";
 import { useEffect, useState } from "react";
 import { useAuth } from "../auth/AuthContext";
+import SetForm from "./SetForm";
 
 export default function RoutineDetails() {
   const { id } = useParams();
   let navigate = useNavigate();
   const [routine, setRoutine] = useState({});
 
-  useEffect(() => {
+  const syncDetails = () => {
     const fetchDetails = async () => {
       const routines = await getRoutines();
       const routine = routines.filter((routine) => routine.id == id)[0];
       setRoutine(routine);
     };
     fetchDetails();
-  }, []);
+  };
+  useEffect(syncDetails, []);
 
   const { token } = useAuth();
 
@@ -39,14 +41,17 @@ export default function RoutineDetails() {
       <h2>{routine.name}</h2>
       <p>{routine.goal}</p>
       <p>Created by {routine.creatorName}</p>
-      {routine.sets?.length > 0 && (
+      {routine.sets?.length > 0 ? (
         <>
           <h3>Sets</h3>
           <ul>{routine.sets.map(SetItem)}</ul>
         </>
+      ) : (
+        <p>Add sets</p>
       )}
-      {token && <button onClick={tryDelete}>Delete</button>}
+      {token && <button onClick={tryDelete}>Delete Routine</button>}
       {error && <p role="alert">{error}</p>}
+      {token && <SetForm syncDetails={syncDetails} routineId={routine.id} />}
     </>
   );
 }
