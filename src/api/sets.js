cae6863b6f648f02898c968d1ebd10b807e761bf
Froggles <elmsterm@gmail.com>
2026-02-11 13:04:24 -0500

    create sets api endpoint

---
+++ src/api/sets.js
@@ -0,0 +1,45 @@
+const API = import.meta.env.VITE_API;
+
+/**
+ * Sends a new set to the API to be created.
+ * A valid token is required.
+ */
+export async function createSet(token, set) {
+  if (!token) {
+    throw Error("You must be signed in to create an set.");
+  }
+
+  const response = await fetch(API + "/sets", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: JSON.stringify(set),
+  });
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.message);
+  }
+}
+
+/**
+ * Requests the API to delete the set with the given ID.
+ * A valid token is required.
+ */
+export async function deleteSet(token, id) {
+  if (!token) {
+    throw Error("You must be signed in to delete an set.");
+  }
+
+  const response = await fetch(API + "/sets/" + id, {
+    method: "DELETE",
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  if (!response.ok) {
+    const result = await response.json();
+    throw Error(result.message);
+  }
+}
