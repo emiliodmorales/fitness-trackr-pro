75037cc946ff12673c28d71d61914c3e7d3e3404
Froggles <elmsterm@gmail.com>
2026-02-11 12:32:43 -0500

    add routine browser routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Route, Routes } from "react-router";
+import ActivitiesPage from "./activities/ActivitiesPage";
+import ActivityDetails from "./activities/ActivityDetails.jsx";
+import RoutinesPage from "./routines/RoutinesPage.jsx";
+import RoutineDetails from "./routines/RoutineDetails.jsx";
+import Layout from "./layout/Layout.jsx";
 import Register from "./auth/Register";
 import Login from "./auth/Login";
-import ActivitiesPage from "./activities/ActivitiesPage";
 import Error404 from "./Error404.jsx";
-import { BrowserRouter, Route, Routes } from "react-router";
-import Layout from "./layout/Layout.jsx";
-import ActivityDetails from "./activities/ActivityDetails.jsx";
 
 /**
  * Fitness Trackr is a platform where fitness enthusiasts can share their workouts and
@@ -17,8 +19,15 @@ export default function App() {
       <Route element={<Layout />}>
         <Route index element={<ActivitiesPage />} />
         <Route path="activities/:id" element={<ActivityDetails />} />
+
+        <Route path="routines">
+          <Route index element={<RoutinesPage />} />
+          <Route path="routines/:id" element={<RoutineDetails />} />
+        </Route>
+
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+
         <Route path="*" element={<Error404 />} />
       </Route>
     </Routes>
